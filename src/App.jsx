1e988cd1ccd4edf150d849b922ff2b54ea92d1c8
moshe peled <moshe.peled@gmail.com>
2025-12-16 16:17:28 +0200

    fix design

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const pages = [
     { id: 1, image: '/images/page1.png', thumbnail: '/images/yoga-tumb.png', title: 'אחים ליוגה', icon: '🧘' },
     { id: 2, image: '/images/page2.png', thumbnail: '/images/card-tumb.png', title: 'מלון תאנה', icon: '🏨' },
-    { id: 3, image: '/images/page3.png', thumbnail: '/images/call-tumb.jpg', title: 'קול קורא', icon: '🎨' },
+    { id: 3, image: '/images/page3.png', thumbnail: '/images/call-tumb.jpg', title: 'קול קורא', icon: '🗣' },
     { id: 4, image: '/images/page4.png', thumbnail: '/images/hotel-tumb.png', title: 'ניהול מלון', icon: '💼' },
   ]
 
